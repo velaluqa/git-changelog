23ef210f6f7be2330ceb011c2ebf4a423725b8aa
Franz Kißig <fkissig@velalu.qa>
2015-07-24 14:28:17 +0200

    Discard multiple commits per range if necessary

--- lib/git/changelog/log.rb
+++ lib/git/changelog/log.rb
@@ -26,37 +26,49 @@ module Git
           while i >= 0 do
             puts "## #{tags[i]}\n\n"
             if i > 0
-              start_predecessor = `git log --format=%H -n 1 #{Shellwords.escape(tags[i-1].to_s)}^2^@`.strip
+              start = `git log --format=%H -n 1 #{Shellwords.escape(tags[i-1].to_s)}^2`.strip
+              start_predecessor = `git log --format=%H -n 1 #{start}^@`.strip
               range = Shellwords.escape "#{start_predecessor}..#{tags[i].to_s}^2"
             else
+              start = `git log --format=%H | tail -n 1`.strip
               range = Shellwords.escape "#{tags[i].to_s}^2"
             end
-            print_commits(`git log --graph --oneline #{range}`)
+            print_commits(`git log --graph --oneline #{range}`, start)
             i -= 1
           end
 
         else
 
-          print_commits(`git log --graph --oneline develop`)
+          print_commits(`git log --graph --oneline develop`, start)
 
         end
       end
 
       private
 
-      def print_commits(log_output)
+      def print_commits(log_output, start)
         log_lines = log_output.split("\n")
-        log_lines.pop
-        log_lines.map! do |log_line|
-          log_line =~ /([^a-f0-9]*[a-f0-9]+ )/
-          unless $1.nil?
-            "`#{log_line[0..$1.length-1]}` #{log_line[$1.length..-1]}"
-          else
-            "`#{log_line}`"
+        reached_start = false
+        output = []
+        log_lines.each do |log_line|
+          # some output lines are tree-only and without commit:
+          # * | 40712a4  Sort curves the same way in legend and graph aside
+          # |/
+          if not reached_start
+            /(?<tree>[^a-f0-9]*)(?:(?<short_rev>[a-f0-9]+)(?<message>.+))?$/ =~ log_line
+            if short_rev.nil?
+              output.push "`#{tree.strip}`"
+            else
+              if start =~ /^#{short_rev}/
+                throw "error" if start.nil?
+                reached_start = true
+              else
+                output.push "`#{tree}#{short_rev}`#{message}"
+              end
+            end
           end
         end
-        log_lines.compact!
-        puts log_lines.join("<br/>\n")
+        puts output.join("<br/>\n")
         puts "\n"
       end
     end
